934b164d74449e7b54b96f78de9dcb96a25f5392
박수현 <soohyun.park@kt.com>
2023-04-30 18:26:13 +0900

    DB 연동 설정 및 JPA 연동 테스트 끝

---
+++ src/main/java/com/example/jpa/repository/MemberRepository.java
@@ -0,0 +1,24 @@
+package com.example.jpa.repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.stereotype.Repository;
+
+import com.example.jpa.model.Member;
+
+@Repository
+public class MemberRepository {
+
+	@PersistenceContext
+	private EntityManager em;
+
+	public Long save(Member member) {
+		em.persist(member); // 저장 후 엔티티를 통째로 리턴하고, 엔티티 수정하면 커맨드가 날아갈 수 있기 때문에, ID만 넘김.
+		return member.getId();
+	}
+
+	public Member find(Long id) {
+		return em.find(Member.class, id);
+	}
+}
---
+++ src/test/java/com/example/jpa/repository/MemberRepositoryTest.java
@@ -0,0 +1,47 @@
+package com.example.jpa.repository;
+
+import javax.transaction.Transactional;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.jpa.model.Member;
+
+@SpringBootTest
+class MemberRepositoryTest {
+
+	@Autowired
+	private MemberRepository repository;
+
+	@Test
+	@Transactional // @Transactional 안붙이면 entitymanager에서 트랜잭션이 없어서 에러남.
+	public void save() throws Exception {
+		// given
+		Member member = new Member();
+		member.setName("박수현");
+
+		// when
+		Long id = repository.save(member);
+		Member findMember = repository.find(id);
+
+		// then
+		Assertions.assertEquals(id, findMember.getId());
+		Assertions.assertEquals(member.getName(), findMember.getName());
+		Assertions.assertEquals(member, findMember); // true.
+		//  ==> 같은 트랜잭션 안에서 저장을 하면 같은 영속성 컨텍스트 안에 있기 때문에, 식별자가 같으면 같은 엔티티.
+		// 1차 캐시에 있기 때문에 select 쿼리도 안날아감.
+
+	}
+
+	@Test
+	@Transactional
+	public void find() throws Exception {
+		// given
+
+		// when
+
+		// then
+	}
+}
