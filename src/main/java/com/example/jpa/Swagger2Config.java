9115b98288aebc1527024e5bbf9d8c997c7cc7ee
박수현 <soohyun.park@kt.com>
2023-05-05 16:02:11 +0900

    swagger 추가

---
+++ src/main/java/com/example/jpa/Swagger2Config.java
@@ -0,0 +1,27 @@
+package com.example.jpa;
+
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class Swagger2Config {
+	@Bean
+	public GroupedOpenApi publicApi() {
+		return GroupedOpenApi.builder()
+			.group("JPA API")
+			.pathsToMatch("/api/**")
+			.build();
+	}
+
+	@Bean
+	public OpenAPI springShopOpenAPI() {
+		return new OpenAPI()
+			.info(new Info().title("JPA API")
+				.description("실전! 스프링부트와 JPA 2 강의 예제 API 명세.")
+				.version("v0.0.1"));
+	}
+}
